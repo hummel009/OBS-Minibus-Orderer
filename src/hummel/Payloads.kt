880cf474081523cb63fa8ba8bcbd1bf6fb52608b
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-06-23 15:54:46 +0300

    Update Payloads.kt

--- src/hummel/Payloads.kt
+++ src/hummel/Payloads.kt
@@ -3,27 +3,27 @@ package hummel
 fun payloadBetweenCities(fromCityID: String, toCityID: String, date: String): String {
 	return """
 		{
-	        "from": "$fromCityID",
-	        "to": "$toCityID",
-	        "date": "$date",
-	        "client": "$phone"
+			"from": "$fromCityID",
+			"to": "$toCityID",
+			"date": "$date",
+			"client": "$phone"
 		}
 	""".trimIndent()
 }
 
 fun payloadBook(fromStopID: String, toStopID: String, timeID: String): String {
 	return """
-        {
-            "client": "$phone",
-            "transfer": "$timeID",
-            "from": "$fromStopID",
-            "to": "$toStopID",
-            "amount": 1,
-            "info": "",
-            "createdBy": {
-                "role": "web-client",
-                "phone": "$phone"
-            }
-        }
-    """.trimIndent()
+		{
+			"client": "$phone",
+			"transfer": "$timeID",
+			"from": "$fromStopID",
+			"to": "$toStopID",
+			"amount": 1,
+			"info": "",
+			"createdBy": {
+				"role": "web-client",
+				"phone": "$phone"
+			}
+		}
+	""".trimIndent()
 }
