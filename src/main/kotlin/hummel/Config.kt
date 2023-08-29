575c506f8841e46fb04715b4387e2cf1e33d5595
Jahor Burboŭski <60618853+Hummel009@users.noreply.github.com>
2023-08-29 14:55:47 +0300

    Update Config.kt

--- src/main/kotlin/hummel/Config.kt
+++ src/main/kotlin/hummel/Config.kt
@@ -3,7 +3,7 @@ package hummel
 val orderingTime: Triple<Int, Int, Int> = Triple(3, 1, 0)
 
 const val phone: String = "+375296186182"
-const val date: String = "2023-06-26"
+const val date: String = "2023-09-04"
 const val time: String = "07:45"
 const val stopFrom: String = "РДК"
 const val stopTo: String = "ст.м.Восток"
