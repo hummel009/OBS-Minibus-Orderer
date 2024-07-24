a9b919a80071b9ee4048d97d32485c9e2c1c6c8f
Hummel009 <hummel.turbamentum@gmail.com>
2024-07-24 17:39:54 +0300

    latest.release

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "2.0.0"
+	id("org.jetbrains.kotlin.jvm") version "latest.release"
 	id("application")
 }
 
@@ -12,15 +12,15 @@ version = LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
 val embed: Configuration by configurations.creating
 
 dependencies {
-	embed("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
-	embed("com.google.code.gson:gson:2.11.0")
-	embed("org.apache.httpcomponents.client5:httpclient5:5.3.1")
-	embed("com.formdev:flatlaf:3.4.1")
-	embed("com.formdev:flatlaf-intellij-themes:3.4.1")
-	implementation("com.google.code.gson:gson:2.11.0")
-	implementation("org.apache.httpcomponents.client5:httpclient5:5.3.1")
-	implementation("com.formdev:flatlaf:3.4.1")
-	implementation("com.formdev:flatlaf-intellij-themes:3.4.1")
+	embed("org.jetbrains.kotlin:kotlin-stdlib:latest.release")
+	embed("com.google.code.gson:gson:latest.release")
+	embed("org.apache.httpcomponents.client5:httpclient5:latest.release")
+	embed("com.formdev:flatlaf:latest.release")
+	embed("com.formdev:flatlaf-intellij-themes:latest.release")
+	implementation("com.google.code.gson:gson:latest.release")
+	implementation("org.apache.httpcomponents.client5:httpclient5:latest.release")
+	implementation("com.formdev:flatlaf:latest.release")
+	implementation("com.formdev:flatlaf-intellij-themes:latest.release")
 }
 
 java {
