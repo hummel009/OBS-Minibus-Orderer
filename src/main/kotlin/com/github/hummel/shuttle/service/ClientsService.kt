2c9d545cfde3cdf92a696f3fa6150ffefbfa9be2
Hummel009 <hummel.turbamentum@gmail.com>
2024-10-15 13:58:29 +0300

    Update ClientsService.kt

--- src/main/kotlin/com/github/hummel/shuttle/service/ClientsService.kt
+++ src/main/kotlin/com/github/hummel/shuttle/service/ClientsService.kt
@@ -5,21 +5,32 @@ import com.github.hummel.shuttle.dao.ClientsDao
 
 object ClientsService {
 	fun unlock(phone: String) {
-		ClientsDao.optionsWithReservations(phone)
+		try {
+			ClientsDao.optionsWithReservations(phone)
+		} catch (e: Exception) {
+			e.printStackTrace()
+		}
 	}
 
 	fun isTicketNotOrdered(
 		cache: Cache, phone: String, token: String, date: String, time: String, stopFromName: String
 	): Boolean {
-		val clientInfo = ClientsDao.getWithReservations(phone, token)
-		val realTime = cache.transfersInfo.find {
-			it.from.time == time
-		}!!.stopsForBooking.find {
-			it.from.name == stopFromName
-		}!!.from.time
+		return try {
+			val clientInfo = ClientsDao.getWithReservations(phone, token)
 
-		return clientInfo.reservations.none {
-			it.date == date && it.from.time == realTime
+			val realTime = cache.transfersInfo.find {
+				it.from.time == time
+			}!!.stopsForBooking.find {
+				it.from.name == stopFromName
+			}!!.from.time
+
+			return clientInfo.reservations.none {
+				it.date == date && it.from.time == realTime
+			}
+		} catch (e: Exception) {
+			e.printStackTrace()
+
+			false
 		}
 	}
 }
